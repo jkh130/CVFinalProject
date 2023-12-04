8e5b03cf2458bee6f79137f92ba20e3ede4c2bbb
jkh130 <jkh130@txstate.edu>
2023-12-03 22:26:27 -0600

    train.py without scikit learn

--- project_files/test.py
+++ project_files/test.py
@@ -3,73 +3,62 @@ import cv2
 import numpy as np
 import joblib
 
+# Custom Decision Stump Classifier
+class DecisionStump:
+    def __init__(self):
+        self.best_feature = None
+        self.best_threshold = None
+        self.best_rule = None
+
+    def predict(self, X):
+        feature_values = X[:, self.best_feature]
+        return np.where(feature_values * self.best_rule < self.best_threshold * self.best_rule, 1, -1)
 
 def detect_skin_ycbcr(image):
-    # Convert the image to YCbCr color space
     ycbcr_image = cv2.cvtColor(image, cv2.COLOR_BGR2YCrCb)
-
-    # Define the range of skin color in YCbCr
     lower_skin = np.array([0, 133, 77], dtype=np.uint8)
     upper_skin = np.array([255, 173, 127], dtype=np.uint8)
-
-    # Threshold the YCbCr image to get only skin colors
     skin_mask = cv2.inRange(ycbcr_image, lower_skin, upper_skin)
-
-    # Bitwise-AND mask and original image
     skin = cv2.bitwise_and(image, image, mask=skin_mask)
-
     return skin, skin_mask
 
 def compute_haar_features(image):
-    # Define the Haar cascade classifier
     face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-
-    # Detect faces in the image
     faces = face_cascade.detectMultiScale(image, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
-
-    # Extract Haar-like features
     features = []
     for (x, y, w, h) in faces:
         features.append([x, y, x + w, y + h])
-
     return features
 
+def adaboost_predict(classifiers, alpha_values, X):
+    final_predictions = [sum(alpha * clf.predict(np.array([x])) for alpha, clf in zip(alpha_values, classifiers)) for x in X]
+    return [1 if prediction > 0 else -1 for prediction in final_predictions]
 
-# Load the trained model
 model_filename = 'face_detection_model.joblib'
 ada_boost_classifier = joblib.load(model_filename)
 
-# Directory containing JPG images for face detection
-test_image_dir = '../training_test_data/test_face_photos/'
-
-# Output directory to save processed images with rectangles
+test_image_dir = 'training_test_data\\test_face_photos'
 output_dir = 'output/'
-
-# Create the output directory if it doesn't exist
 os.makedirs(output_dir, exist_ok=True)
 
-# List all JPG files in the test directory
 test_jpg_files = [f for f in os.listdir(test_image_dir) if f.endswith('.jpg') or f.endswith('JPG')]
 
-# Iterate through JPG files in the test directory and apply face detection
 for jpg_file in test_jpg_files:
     image_path = os.path.join(test_image_dir, jpg_file)
     image = cv2.imread(image_path)
 
-    # Apply skin detection
     skin_detected, skin_mask = detect_skin_ycbcr(image)
-
-    # Convert skin-detected image to grayscale for Haar feature computation
     gray_skin = cv2.cvtColor(skin_detected, cv2.COLOR_BGR2GRAY)
-
-    # Detect faces using the loaded AdaBoost classifier on the skin-filtered image
     faces = compute_haar_features(gray_skin)
 
-    # Draw rectangles around detected faces on the original image
-    for (x, y, x2, y2) in faces:
-        cv2.rectangle(image, (x, y), (x2, y2), (0, 255, 0), 2)
+    X_test = np.array(faces, dtype=np.float32)
+    predictions = adaboost_predict(ada_boost_classifier[0], ada_boost_classifier[1], X_test)
 
-    # Save the processed image in the output directory
+    for face, prediction in zip(faces, predictions):
+        if prediction == 1:
+            x, y, x2, y2 = face
+            cv2.rectangle(image, (x, y), (x2, y2), (0, 255, 0), 2)
+
     output_path = os.path.join(output_dir, jpg_file)
     cv2.imwrite(output_path, image)
 
--- project_files/train.py
+++ project_files/train.py
@@ -1,56 +1,145 @@
-import os
-import cv2
 import numpy as np
-from sklearn.ensemble import AdaBoostClassifier
-from sklearn.tree import DecisionTreeClassifier
+import cv2
+import os
 import joblib
+class DecisionStump:
+    def __init__(self):
+        self.best_feature = None
+        self.best_threshold = None
+        self.best_rule = None
+
+    def train(self, X, y, weights):
+        num_features = X.shape[1]
+        min_error = float('inf')
+
+        for feature in range(num_features):
+            feature_values = X[:, feature]
+            thresholds = np.unique(feature_values)
+            
+            for threshold in thresholds:
+                for rule in [1, -1]:
+                    predictions = np.where(feature_values * rule < threshold * rule, 1, -1)
+                    misclassified = predictions != y
+                    weighted_error = np.sum(weights[misclassified])
+
+                    if weighted_error < min_error:
+                        min_error = weighted_error
+                        self.best_feature = feature
+                        self.best_threshold = threshold
+                        self.best_rule = rule
+
+    def predict(self, X):
+        feature_values = X[:, self.best_feature]
+        return np.where(feature_values * self.best_rule < self.best_threshold * self.best_rule, 1, -1)
+    
 
 # Function to compute Haar-like features using OpenCV
 def compute_haar_features(image):
-    # Define the Haar cascade classifier (you can use pre-trained cascades or train your own)
     face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-
-    # Detect faces in the image
-    faces = face_cascade.detectMultiScale(image, scaleFactor=1.1, minNeighbors=10, minSize=(40, 40))
-
-    # Extract Haar-like features
+    faces = face_cascade.detectMultiScale(image, scaleFactor=1.2, minNeighbors=20, minSize=(30, 30))
     features = []
     for (x, y, w, h) in faces:
-        features.append([x, y, x + w, y + h])  # Rectangular region of detected face
-
+        features.append([x, y, x + w, y + h])
     return features
 
-# Directory containing BMP face images for training
-train_image_dir = '../training_test_data/training_faces/'
+# Load and preprocess the data
+def load_data(train_face_dir, train_nonface_dir):
+    # Load face images
+    train_face_files = [f for f in os.listdir(train_face_dir) if f.endswith('.bmp')]
+    X_train_face = []
+    for bmp_file in train_face_files:
+        image_path = os.path.join(train_face_dir, bmp_file)
+        image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+        features = compute_haar_features(image)
+        X_train_face.extend(features)
 
-# List all BMP files in the training directory
-train_bmp_files = [f for f in os.listdir(train_image_dir) if f.endswith('.bmp')]
+    # Load non-face images
+    train_nonface_files = [f for f in os.listdir(train_nonface_dir) if f.endswith('.jpg') or f.endswith('.JPG')]
+    X_train_nonface = []
+    for jpg_file in train_nonface_files:
+        image_path = os.path.join(train_nonface_dir, jpg_file)
+        image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+        X_train_nonface.append([0, 0, image.shape[1], image.shape[0]])  # Use the whole image as a non-face feature
 
-# Initialize lists for training features and labels
-X_train = []
-y_train = []
+    # Combine face and non-face data
+    X_train = X_train_face + X_train_nonface
+    y_train = [1] * len(X_train_face) + [-1] * len(X_train_nonface)  # 1 for faces, -1 for non-faces
 
-# Iterate through BMP files in the training directory and extract features
-for bmp_file in train_bmp_files:
-    image_path = os.path.join(train_image_dir, bmp_file)
-    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    return np.array(X_train, dtype=np.float32), np.array(y_train, dtype=np.int32)
 
-    # Extract Haar-like features from the image
-    features = compute_haar_features(image)
 
-    X_train.extend(features)
-    y_train.extend([1] * len(features))  # Assuming all detected regions are faces
+def initialize_weights(n):
+    return np.ones(n) / n
 
-# Convert the training feature list to a NumPy array
-X_train = np.array(X_train, dtype=np.float32)
-y_train = np.array(y_train, dtype=np.int32)
+def train_weak_classifier(X, y, weights):
+    stump = DecisionStump()
+    stump.train(X, y, weights)
+    return stump
 
-# Train the AdaBoost classifier
-ada_boost_classifier = AdaBoostClassifier(base_estimator=DecisionTreeClassifier(max_depth=1), n_estimators=50)
-ada_boost_classifier.fit(X_train, y_train)
 
-# Save the trained model to a file using joblib
+
+def calculate_error_and_alpha(classifier, X, y, weights):
+    # Make predictions using the classifier
+    predictions = classifier.predict(X)
+
+    # Determine which predictions are incorrect
+    incorrect = predictions != y
+
+    # Calculate weighted error
+    error = np.sum(weights[incorrect]) / np.sum(weights)
+
+    # Avoid division by zero; handle edge cases
+    error = min(max(error, 1e-10), 1 - 1e-10)
+
+    # Calculate alpha
+    alpha = 0.5 * np.log((1 - error) / error)
+
+    return error, alpha
+
+
+def update_weights(weights, alpha, classifier, X, y):
+    # Make predictions using the classifier
+    predictions = classifier.predict(X)
+
+    # Determine whether each prediction is correct
+    correct = predictions == y
+
+    # Update weights: increase for misclassified, decrease for correctly classified
+    weights *= np.exp(alpha * np.where(correct, -1, 1))
+
+    # Normalize the weights so they sum up to 1
+    weights /= np.sum(weights)
+
+    return weights
+
+# AdaBoost training process
+def adaboost(X, y, num_classifiers):
+    n = len(y)
+    weights = initialize_weights(n)
+    classifiers = []
+    alpha_values = []
+
+    for _ in range(num_classifiers):
+        classifier = train_weak_classifier(X, y, weights)
+        error, alpha = calculate_error_and_alpha(classifier, X, y, weights)
+        weights = update_weights(weights, alpha, classifier, X, y)
+        classifiers.append(classifier)
+        alpha_values.append(alpha)
+
+    return classifiers, alpha_values
+
+# Prediction using AdaBoost
+def adaboost_predict(classifiers, alpha_values, x, threshold=0):
+    final_prediction = sum(alpha * clf.predict(x) for alpha, clf in zip(alpha_values, classifiers))
+    return 1 if final_prediction > threshold else -1
+
+# Main execution
+train_face_dir = 'training_test_data/training_faces'
+train_nonface_dir = 'training_test_data/training_nonfaces'
+X_train, y_train = load_data(train_face_dir, train_nonface_dir)
+classifiers, alpha_values = adaboost(X_train, y_train, num_classifiers=30)
+
+# Save the AdaBoost model
 model_filename = 'face_detection_model.joblib'
-joblib.dump(ada_boost_classifier, model_filename)
+joblib.dump((classifiers, alpha_values), model_filename)
 
-print(f"Model saved to {model_filename}")
