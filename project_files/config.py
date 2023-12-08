2facce4fb9750f8c36e1848d3e338732cbc95ef4
jkh130 <jkh130@txstate.edu>
2023-12-07 18:44:21 -0600

    config

---
+++ project_files/config.py
@@ -0,0 +1,3 @@
+data_directory = 'E:\\FinalSemesterResources\\Operating Systems\\Project2Github\\CVFinalProject\\project_files\\data'
+code_directory = 'E:\\FinalSemesterResources\\Operating Systems\\Project2Github\CVFinalProject\project_files'
+training_directory = 'E:\\FinalSemesterResources\\Operating Systems\\Project2Github\\CVFinalProject\\training_test_data'
